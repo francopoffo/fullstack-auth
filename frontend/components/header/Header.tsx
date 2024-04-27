58d9f04eb277e7c9b16e1163b30d472f34cf7193
Franco Poffo <franco_sutter@yahoo.com.br>
2024-04-27 14:38:24 -0300

    beginning of header

---
+++ frontend/components/header/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between py-4 px-24 border-b-2 border-slate-300 bg-slate-400 min-w-full">
+      <div>
+        <h1 className="text-3xl font-bold px-4">Fullstack Auth Demo</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
