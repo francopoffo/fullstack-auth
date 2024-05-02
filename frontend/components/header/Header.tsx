bd84c7e47ddee3bd84da5771779c2639a55df6f6
Franco Poffo <franco_sutter@yahoo.com.br>
2024-05-02 19:31:07 -0300

    frontend/style: adjusting header position

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-4 px-24 border-b-2 border-slate-300 bg-slate-400 min-w-full">
+    <header className="flex items-center justify-between py-4 px-36 border-b-2 border-slate-300 bg-slate-400 min-w-full fixed top-0 left-0 right-0">
       <Link href="/">
         {" "}
         <h1 className="text-3xl font-bold px-4">Fullstack Auth Demo</h1>
