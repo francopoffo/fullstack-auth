c094d17d4e23d1d81ed39346af4926f5a7e29f55
Franco Poffo <franco_sutter@yahoo.com.br>
2024-05-02 11:54:46 -0300

    frontend: basic register/login form structures

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-4 px-24 border-b-2 border-slate-300 bg-slate-400 min-w-full">
-      <div>
+      <Link href="/">
+        {" "}
         <h1 className="text-3xl font-bold px-4">Fullstack Auth Demo</h1>
-      </div>
+      </Link>
+      <Link href="/login">
+        <Button variant="default">Login</Button>
+      </Link>
     </header>
   );
 };
---
+++ frontend/components/login/LoginForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(2, { message: "Password is required." }),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="your username" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="your password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex flex-col items-center justify-between">
+          <Button className="self-end" type="submit">
+            Submit
+          </Button>
+          <Button variant="link" className="text-sm p-2">
+            <Link href="/register">Not yet registered?</Link>
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
